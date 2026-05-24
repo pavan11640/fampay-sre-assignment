e9c3ab541ac41a3c7fdac9f9b4e50bc25450301a
chandrapavan.vajji@infor.com <chandrapavan.vajji@infor.com>
2026-05-24 19:45:52 +0530

    Fix: upgrade EKS to K8s 1.30 for AMI support

--- environments/production/variables.tf
+++ environments/production/variables.tf
@@ -31,7 +31,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.28"
+  default     = "1.30"
 }
 
 variable "node_instance_types" {
