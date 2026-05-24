3421afb1faf629e709f369092984028a5db9fe48
chandrapavan.vajji@infor.com <chandrapavan.vajji@infor.com>
2026-05-24 22:12:24 +0530

    Fix: use K8s 1.31 (fresh cluster, latest supported AMI)

--- environments/production/variables.tf
+++ environments/production/variables.tf
@@ -31,7 +31,7 @@ variable "cluster_name" {
 variable "cluster_version" {
   description = "Kubernetes version"
   type        = string
-  default     = "1.29"
+  default     = "1.31"
 }
 
 variable "node_instance_types" {
